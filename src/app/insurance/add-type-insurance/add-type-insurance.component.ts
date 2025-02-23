db718188bcdce913e7a2a13230fdbeb3ceabad23
Elvi Budlla <elvibudlla09@gmail.com>
2025-02-23 15:12:18 +0100

    Insurance Component

---
+++ src/app/insurance/add-type-insurance/add-type-insurance.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-type-insurance',
+  imports: [],
+  templateUrl: './add-type-insurance.component.html',
+  styleUrl: './add-type-insurance.component.scss'
+})
+export class AddTypeInsuranceComponent {
+
+}
---
+++ src/app/insurance/calculate-insurance/calculate-insurance.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculate-insurance',
+  imports: [],
+  templateUrl: './calculate-insurance.component.html',
+  styleUrl: './calculate-insurance.component.scss'
+})
+export class CalculateInsuranceComponent {
+
+}
