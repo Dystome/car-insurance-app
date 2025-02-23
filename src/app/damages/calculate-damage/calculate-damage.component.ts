63b4f7a0710ce395962e69f7452e8aab824e57c2
Eva Mertiri <evamertiri@gmail.com>
2025-02-23 15:03:05 +0100

    damageComponent

---
+++ src/app/damages/calculate-damage/calculate-damage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculate-damage',
+  imports: [],
+  templateUrl: './calculate-damage.component.html',
+  styleUrl: './calculate-damage.component.scss'
+})
+export class CalculateDamageComponent {
+
+}
