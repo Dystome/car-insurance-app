eccd37283878059669bd39f5865edcce8cbd5d90
Dystome <94131597+Dystome@users.noreply.github.com>
2025-03-02 11:05:17 +0100

    fixed routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,24 @@
-import { Routes } from '@angular/router';
-import { UsersComponent } from './modules/users/users.component';
+import { AppComponent } from './app.component';
+import { AddDamagesComponent } from './damages/add-damage/add-damages.component';
+import { AddTypeOfDamageComponent } from './damages/add-type-of-damage/add-type-of-damage.component';
+import { CalculateDamageComponent } from './damages/calculate-damage/calculate-damage.component';
+import { AlldemagesComponent } from './demages/alldemages/alldemages.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './modules/login/login.component';
+import { SignupComponent } from './modules/signup/signup.component';
 
 export const routes: Routes = [
-  { path: "users-component", component: UsersComponent },
+  { path: '', redirectTo: '/signup', pathMatch: 'full' },
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+  { path: "app-component", component: AppComponent },
+  { path: "add-demage", component: AddDamagesComponent },
+  {path: "add-type-demage" , component:AddTypeOfDamageComponent},
+  {path:"calculate-demage" ,  component : CalculateDamageComponent},
+  {path:"all-demages" ,  component : AlldemagesComponent},
   { path: "**", redirectTo: "users-component" }
+
 ];
 
 @NgModule({
